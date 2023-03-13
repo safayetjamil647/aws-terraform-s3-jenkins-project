1145fa97ff162e06e84c6551d4f67fc9a47d142f
safayetjamil647 <work.jamil4@gmail.com>
2023-03-13 19:24:55 +0600

    backend modified

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aws-terraform-s3-jenkins-project"
+    bucket = "aws-terraform-s3-jenkins-project-jamil"
     key    = "remote.tfstate"
     region = "us-east-1"
   }
